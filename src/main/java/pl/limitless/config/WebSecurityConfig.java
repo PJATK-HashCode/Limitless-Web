099291975361466b9319d2b09f30a23dbfc80e3d
jMerta <mer.kuba@gmail.com>
2017-06-09 18:34:02 +0200

    hibernate

--- src/main/java/pl/limitless/config/WebSecurityConfig.java
+++ src/main/java/pl/limitless/config/WebSecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
 
     }
 
