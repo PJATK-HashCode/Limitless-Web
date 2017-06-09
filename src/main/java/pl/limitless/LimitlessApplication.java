a9c07a298b28e513e1f8e98be3a0f5f789b66889
jMerta <mer.kuba@gmail.com>
2017-06-09 13:00:16 +0200

    Hello world!

---
+++ src/main/java/pl/limitless/LimitlessApplication.java
@@ -0,0 +1,12 @@
+package pl.limitless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitlessApplication.class, args);
+	}
+}
