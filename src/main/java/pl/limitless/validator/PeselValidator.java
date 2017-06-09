1fa44d950779dfe1fbe4fee7fcca1bf5e6e87d6f
Lelental <p.lelental@gmail.com>
2017-06-09 17:14:46 +0200

    pesel #2

--- src/main/java/pl/limitless/validator/PeselValidator.java
+++ src/main/java/pl/limitless/validator/PeselValidator.java
@@ -4,20 +4,26 @@ package pl.limitless.validator;
  * @author Lelental on 09.06.2017.
  */
 public class PeselValidator {
-    private boolean checkPesel(String pesel){
 
-        int weight[] = {1,3,7,9,1,3,7,9,1,3};
+    public static boolean checkPesel(String pesel) {
+
+        int weight[] = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
         int sum = 0;
 
-        if(pesel.length() != 11){
+        if (pesel.length() != 11) {
             return false;
         }
 
-        for(int i =0; i<10;i++){
-            sum += Integer.parseInt(pesel.substring(i,i+1))*weight[i];
+        for (int i = 0; i < 10; i++) {
+            sum += Integer.parseInt(pesel.substring(i, i + 1)) * weight[i];
         }
 
-        int checkNumber = Integer.parseInt(pesel.substring(10,11));
+        int checkNumber = Integer.parseInt(pesel.substring(10, 11));
 
+        sum %= 10;
+        sum = 10 - sum;
+        sum %= 10;
+
+        return (sum == checkNumber);
     }
 }
